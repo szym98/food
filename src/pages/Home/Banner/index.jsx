c2bb2a6a7e5769d6b211dc9cbd5fe20b08904483
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-03-31 19:18:47 +0200

    demo

--- src/pages/Home/Banner/index.jsx
+++ src/pages/Home/Banner/index.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import "./style.css";
+import bannerImg from "./banner.png"
+import Logo from "../../../components/common/Logo";
 
 const Banner = () => {
   return (
     <header>
       <div className="header-content"></div>
-      <img src={} className="header-images"/>
+        <Logo/>
+        <div className="content-main">
+            <h1>Delicious food for your cravings</h1>
+            <p> We made fresh and healthy meals</p>
+            <button>View menu <i className='fas fa-long-arrow-alt-right'></i>
+            </button>
+
+        </div>
+      <img src={bannerImg} alt="banner" className="header-images"/>
     </header>
   );
 };
