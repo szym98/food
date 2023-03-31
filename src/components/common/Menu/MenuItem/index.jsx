a0614086ffc123c87be89f86dad1436435299afd
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-04-01 01:48:28 +0200

    demo

--- src/components/common/Menu/MenuItem/index.jsx
+++ src/components/common/Menu/MenuItem/index.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
-import './style.css';
-const MenuItem=(id,name,info,price,img)=>{
-    return(<div>
-            <p>Menu items</p>
+import React from "react";
+import "./style.css";
+const MenuItem =( {item}) => {
+    const {id, name, info, price, img}=item
+  return (
+    <div className="item">
+      <img src={img} alt="item" />
+      <div className="item-head_desc">
+        <p className="head_desc-name">{name}</p>
+        <p className="head_desc-info">
+          <small>{info}</small>
+        </p>
+        <div className="item-foot_desc">
+            <span className="item-foot_desc-price">${price}</span>
+            <button>Add +</button>
         </div>
-
-    );
+      </div>
+    </div>
+  );
 };
 export default MenuItem;
