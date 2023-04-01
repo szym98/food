ad0f299839e415257897d6df82908a96bc33f311
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-04-02 00:07:35 +0200

    demo

--- src/components/common/CartCountButton/index.jsx
+++ src/components/common/CartCountButton/index.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 import './style.css';
 
-const CartCountButton=({cartCount})=>{
-    return(
-        <div className='btnCartCount'>
-            <div className='count'>{cartCount >= 100? '99+' :cartCount}</div>
+const CartCountButton = ({ cartCount }) => {
+    const navigate = useNavigate();
+    return (
+        <div className='btnCartCount' onClick={() => navigate('/cart')}>
+            <div className='count'>{cartCount >= 100 ? '99+' : cartCount}</div>
             <i className='fas fa-shopping-cart'></i>
         </div>
     );
 };
+
 export default CartCountButton;
