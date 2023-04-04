8b92adc6506f4c78e728aa45d70ae0338075f203
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-04-04 11:51:05 +0200

    demo

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import {persistor,store} from "./redux/store";
+import { persistor, store } from "./redux/store";
 
 ReactDOM.render(
   <Provider store={store}>
