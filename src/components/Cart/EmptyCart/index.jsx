6ce1077d506e35a4dc1979c5bac872922fe86a79
Szymon Naculak <114177495+szym98@users.noreply.github.com>
2023-04-04 11:55:15 +0200

    demo

--- src/components/Cart/EmptyCart/index.jsx
+++ src/components/Cart/EmptyCart/index.jsx
@@ -4,15 +4,15 @@ import emptyCartImg from "./emptyCat.png";
 import { useNavigate } from "react-router-dom";
 
 const EmptyCart = () => {
-    const navigate = useNavigate();
-    return (
-        <div className="emptyCart">
-            <img src={emptyCartImg} alt="empty" />
-            <button onClick={() => navigate("/")}>
-                <i className="fas fa-long-arrow-alt-left"></i> Return to The Shop
-            </button>
-        </div>
-    );
+  const navigate = useNavigate();
+  return (
+    <div className="emptyCart">
+      <img src={emptyCartImg} alt="empty" />
+      <button onClick={() => navigate("/")}>
+        <i className="fas fa-long-arrow-alt-left"></i> Back to the shop
+      </button>
+    </div>
+  );
 };
 
 export default EmptyCart;
